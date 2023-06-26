1fbeaaeabfa339ffc21f391a29e1671aee6be80c
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-26 14:27:12 -0700

    fixed error with line count

--- utils/db.js
+++ utils/db.js
@@ -6,9 +6,8 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
 
-    this.client = new MongoClient(`mongodb://${host}:${port}`, {
-	    useUnifiedTopology: true
-    });
+    this.client = new MongoClient(`mongodb://${host}:${port}`,
+	    { useUnifiedTopology: true });
     this.database = database;
   }
 
