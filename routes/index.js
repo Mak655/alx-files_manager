e0356d35ffa8049bd3b147f1a2f38e3f74bc0bb7
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-30 22:03:05 -0700

    fix_routes

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
+const FilesController = require('../controllers/FilesController');
 
+
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
